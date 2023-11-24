05bb2debecbe0f94283dd9ca150b243719273172
dngst <dngst@protonmail.com>
2023-11-24 15:35:25 +0300

    Add string to array

---
+++ string_to_array.rb
@@ -0,0 +1,9 @@
+def string_to_array(string)
+  string.split(" ")
+end
+
+p string_to_array("Robin Singh")
+p string_to_array("CodeWars")
+p string_to_array("I love arrays they are my favorite")
+p string_to_array("")
+p string_to_array("1 2 3")
