b78483f5ca4e23b45630c1dc185573ff9a55de37
dngst <dngst@protonmail.com>
2023-12-01 21:13:35 +0300

    Rename method

--- fundamentals/is_isogram.rb
+++ fundamentals/is_isogram.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-def is_isogram(string)
+def isogram?(string)
   chars = string.downcase.chars
   chars.uniq == chars
 end
 
-p is_isogram('Dermatoglyphics') # true
-p is_isogram('isogram') # true
-p is_isogram('aba') # false, "same chars may not be adjacent"
-p is_isogram('moOse') # false, "same chars may not be same case"
-p is_isogram('isIsogram') # false
-p is_isogram('') # true, "an empty string is a valid isogram"
+p isogram?('Dermatoglyphics') # true
+p isogram?('isogram') # true
+p isogram?('aba') # false, "same chars may not be adjacent"
+p isogram?('moOse') # false, "same chars may not be same case"
+p isogram?('isIsogram') # false
+p isogram?('') # true, "an empty string is a valid isogram"
