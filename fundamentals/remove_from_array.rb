081d0fd3ee8d63b047919ad793d252c231ba80c3
dngst <dngst@protonmail.com>
2023-12-03 20:54:15 +0300

    Finish fundamentals/remove_from_array.rb

--- fundamentals/remove_from_array.rb
+++ fundamentals/remove_from_array.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 def remove_every_other(arr)
-  arr.select { |r| r != arr[1] }
+  arr.select.with_index { |_, index| index.even? }
 end
 
 p remove_every_other(['Hello', 'Goodbye', 'Hello Again']) # ['Hello', 'Hello Again'])
 p remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) # [1, 3, 5, 7, 9]
 p remove_every_other([[1, 2]]) # [[1, 2]]
-p remove_every_other([['Goodbye'], {'Great': 'Job'}]) # [['Goodbye']]
+p remove_every_other([['Goodbye'], { Great: 'Job' }]) # [['Goodbye']]
 p remove_every_other([]) # []
