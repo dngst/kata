8c312ee44c9ac8ad68c1765ac5125e86a2074ee3
dngst <dngst@protonmail.com>
2023-12-04 22:34:52 +0300

    Add count_chars

---
+++ fundamentals/count_chars.rb
@@ -0,0 +1,9 @@
+def count_chars(str)
+  # str.chars.tally
+  str.chars.each_with_object({}) do |char, my_hash|
+    my_hash[char] = str.chars.count(char)
+  end
+end
+
+p count_chars("aba") # {"a" => 2, "b" => 1}
+p count_chars("") # {}
